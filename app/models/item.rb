4b0a2bae17409c658149215a7684b3e291121bdc
ryuseinii <ryuseinii5519@gmail.com>
2023-11-14 21:24:31 +0900

    ActiveHashの導入

--- app/models/item.rb
+++ app/models/item.rb
@@ -1,4 +1,16 @@
 class Item < ApplicationRecord
+  extend ActiveHash::Associations::ActiveRecordExtensions
+
+  # テーブルとのアソシエーション
   belongs_to :user
+
+  # アクティブハッシュとのアソシエーション
+  belongs_to :category
+  belongs_to :prefecture
+  belongs_to :status
+  belongs_to :shipping_charge
+  belongs_to :shipping_days
+
+  # active_storageとのアソシエーション
   has_one_attached :image
 end
