d227e344d8c2fd382bea847520c462367d9cd921
ryuseinii <ryuseinii5519@gmail.com>
2023-11-15 20:23:06 +0900

    バリデーション設定

--- app/models/item.rb
+++ app/models/item.rb
@@ -13,4 +13,27 @@ class Item < ApplicationRecord
 
   # active_storageとのアソシエーション
   has_one_attached :image
+
+  with_options presence: true do
+    validates :user_id
+    validates :image
+    validates :name
+    validates :description
+    validates :category_id
+    validates :status_id
+    validates :shipping_charge_id
+    validates :prefecture_id
+    validates :shipping_days_id
+    # 300円以上かつ9,999,999円以下で、半角数字でないと入力不可
+    validates :price, numericality: { greater_than_or_equal_to: 300, less_than_or_equal_to: 9_999_999 }
+  end
+
+  # ジャンルの選択が「--」の時は保存不可
+  with_options numericality: { other_than: 0 } do
+    validates :category_id
+    validates :prefecture_id
+    validates :status_id
+    validates :shipping_charge_id
+    validates :shipping_days_id
+  end
 end
