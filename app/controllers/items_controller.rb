59c1c9a076596fd627e9eb6b21fe860ee95f573a
ryuseinii <ryuseinii5519@gmail.com>
2023-11-15 20:36:00 +0900

    商品出品ページ表示

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -3,6 +3,11 @@ class ItemsController < ApplicationController
   before_action :authenticate_user!, except: :index
 
   def index
+    @items = Item.order('created_at DESC')
+  end
+
+  def new
+    @item = Item.new
   end
 
   private
