ea69a3e8bee78d456c18480734085de95800c2fb
ryuseinii <ryuseinii5519@gmail.com>
2023-11-18 16:08:52 +0900

    手数料計算

--- app/javascript/item_price.js
+++ app/javascript/item_price.js
@@ -1,12 +1,12 @@
 window.addEventListener('load', () => {
   const priceInput = document.getElementById("item-price");
-  priceInput.addEventListener("input", () => {
-  const inputValue = priceInput.value;
-  const addTaxDom = document.getElementById("add-tax-price");
-  const ProfitDom = document.getElementById("profit");
-  // 入力した金額をもとに販売手数料を計算する処理
-  addTaxDom.innerHTML = Math.floor(inputValue * 0.1);
-  // 出品価格から販売手数料を引く処理
-  ProfitDom.innerHTML = inputValue - addTaxDom.innerHTML;
-  })
+ //  console.log(priceInput)
+   priceInput.addEventListener("input", () => {
+     const addTaxDom = document.getElementById("add-tax-price");
+     // console.log(priceInput.innerHTML);
+     addTaxDom.innerHTML = Math.round(priceInput.value * 0.1 );
+     const addPofitDom = document.getElementById("profit");
+     // console.log(priceInput.innerHTML);
+     addPofitDom.innerHTML = Math.round(priceInput.value - Math.round(priceInput.value * 0.1 ))
+ })
 });
