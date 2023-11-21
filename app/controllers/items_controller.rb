6190c0c875579b533320e0e3e55d4083b06a9621
ryuseinii <ryuseinii5519@gmail.com>
2023-11-21 21:39:24 +0900

    特定のレコード取得

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -44,8 +44,9 @@ class ItemsController < ApplicationController
 
   def destroy
     # ログインしているユーザーと同一であればデータを削除する
-    if @item.user_id == current_user.id
-      @item.destroy
+    item = Item.find(params[:id])
+    if item.user_id == current_user.id
+      item.destroy
       redirect_to root_path
     else
       redirect_to root_path
