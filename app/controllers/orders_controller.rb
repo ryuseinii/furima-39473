36b1c64ede0bd97caac13e33cfaecad30c15a9d5
ryuseinii <ryuseinii5519@gmail.com>
2023-11-23 21:17:23 +0900

    保存ができるよう実装

--- app/controllers/orders_controller.rb
+++ app/controllers/orders_controller.rb
@@ -1,9 +1,23 @@
 class OrdersController < ApplicationController
 
   def index
+    @order_form = OrderForm.new
   end
 
   def create
+    @order_form = OrderForm.new(order_params)
+    if @order_form.valid?
+      pay_item
+      @order_form.save
+      redirect_to root_path
+    else
+      render :index
+    end
   end
-  
+
+  private
+
+  def order_params
+    params.require(:order_form).permit(:postcode, :prefecture_id, :city, :block, :building, :phone_number).merge(user_id: current_user.id, item_id: params[:item_id], token: params[:token])
+  end
 end
