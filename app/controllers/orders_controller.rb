085de0f14d45ffcb5e0d2e98f4631337a17911da
ryuseinii <ryuseinii5519@gmail.com>
2023-11-26 22:24:36 +0900

    Formオブジェクト実装
    Formオブジェクトを用いて、購入時に複数のテーブルへ情報が保存できるようにする

--- app/controllers/orders_controller.rb
+++ app/controllers/orders_controller.rb
@@ -1,4 +1,6 @@
 class OrdersController < ApplicationController
+  before_action :authenticate_user!
+  before_action :non_purchased_item, only: [:index, :create]
 
   def index
     @order_form = OrderForm.new
@@ -19,5 +21,10 @@ class OrdersController < ApplicationController
 
   def order_params
     params.require(:order_form).permit(:postcode, :prefecture_id, :city, :block, :building, :phone_number).merge(user_id: current_user.id, item_id: params[:item_id], token: params[:token])
+  end
+
+  def non_purchased_item
+    @item = Item.find(params[:item_id])
+    redirect_to root_path if current_user.id == @item.user_id || @item.order.present?
   end
 end
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.0].define(version: 2023_11_23_120039) do
+ActiveRecord::Schema[7.0].define(version: 2023_11_26_115710) do
   create_table "active_storage_attachments", charset: "utf8", force: :cascade do |t|
     t.string "name", null: false
     t.string "record_type", null: false
@@ -54,6 +54,11 @@ ActiveRecord::Schema[7.0].define(version: 2023_11_23_120039) do
     t.index ["user_id"], name: "index_items_on_user_id"
   end
 
+  create_table "order_forms", charset: "utf8", force: :cascade do |t|
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
+
   create_table "orders", charset: "utf8", force: :cascade do |t|
     t.bigint "user_id", null: false
     t.bigint "item_id", null: false
@@ -61,6 +66,19 @@ ActiveRecord::Schema[7.0].define(version: 2023_11_23_120039) do
     t.datetime "updated_at", null: false
     t.index ["item_id"], name: "index_orders_on_item_id"
     t.index ["user_id"], name: "index_orders_on_user_id"
+  end
+
+  create_table "payments", charset: "utf8", force: :cascade do |t|
+    t.bigint "order_id", null: false
+    t.string "postcode", null: false
+    t.integer "prefecture_id", null: false
+    t.string "city", null: false
+    t.string "block", null: false
+    t.string "building"
+    t.string "phone_number", null: false
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.index ["order_id"], name: "index_payments_on_order_id"
   end
 
   create_table "users", charset: "utf8", force: :cascade do |t|
@@ -86,4 +104,5 @@ ActiveRecord::Schema[7.0].define(version: 2023_11_23_120039) do
   add_foreign_key "items", "users"
   add_foreign_key "orders", "items"
   add_foreign_key "orders", "users"
+  add_foreign_key "payments", "orders"
 end
